d3d29f8a6fa5b497c887bcc06e127aebfe860aa0
user <toehtet.h.lwin@frontiir.net>
2025-01-17 16:08:37 +0630

    update

--- src/app/messenger/create/create.component.ts
+++ src/app/messenger/create/create.component.ts
@@ -1,81 +1,22 @@
-import { Component, computed } from '@angular/core';
+import { Component} from '@angular/core';
 import { ButtonModule } from 'primeng/button';
-import { DropdownModule } from 'primeng/dropdown';
 import { InputTextModule } from 'primeng/inputtext';
-import { Router } from '@angular/router';
-import {
-  FormControl,
-  FormGroup,
-  FormsModule,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
-
-import { FileUploadModule } from 'primeng/fileupload';
-
+import { FormsModule } from '@angular/forms';
 import { CardModule } from 'primeng/card';
-import { CommonModule } from '@angular/common';
 
 @Component({
   standalone: true,
-  selector: 'app-notification-create',
+  selector: 'app-create',
   templateUrl: './create.component.html',
   styleUrls: ['./create.component.scss'],
-  imports: [
-    DropdownModule,
-    InputTextModule,
-    ButtonModule,
-    FormsModule,
-    ReactiveFormsModule,
-    FileUploadModule,
-    CardModule,
-    CommonModule,
-  ],
+  imports: [InputTextModule, ButtonModule, FormsModule, CardModule],
 })
 export class CreateComponent {
-  public langData: any;
-  public createForm!: FormGroup;
   public payload: any;
+  value: string = '';
 
-  public employeeList: any;
+  constructor() {}
 
-  constructor(private _router: Router) {}
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-    // this.checkValidationForm();
-    this.fetchData();
-    this.initSubscribe();
-  }
-
-  /**
-   * Initialize the  Subscription
-   */
-  initSubscribe(): void {}
-
-  /**
-   * Check Form Validation for create
-   */
-  // checkValidationForm(): void {
-  //   this.createForm = new FormGroup({
-  //     from: new FormControl('', Validators.required),
-  //     to: new FormControl('', Validators.required),
-  //     cc: new FormControl('', Validators.required),
-  //     subject: new FormControl('', Validators.required),
-  //     message: new FormControl('', Validators.required),
-  //   });
-  // }
-
-  /**
-   * Fetch All Data
-   */
-  fetchData(): void {}
-
-  /**
-   * Retrieve Employee List from dropdown
-   */
-
-  /**
-   * Show messgae when click on send button
-   */
-  createNotification(): void {}
 }
